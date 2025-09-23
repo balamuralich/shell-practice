87065fc3892aa1cae9d185fc2cd10c89b1a34b18
jyobala <jyoba@jyotsnach.>
2025-09-24 05:04:56 +0530

    my first script

---
+++ 01-hello-world.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1="Raghu"
+PERSON2="Ramaiah"
+PERSON3="Satish"
+
+echo "$PERSON1:: How are you $PERSON2"
+echo "$PERSON2:: I am fine, $PERSON1 what are you doing now"
+echo "$PERSON1:: I think your job was confirmed, then $PERSON3 will be happy."
+echo "$PERSON2:: Yes $PERSON3 is very happy $PERSON1"
