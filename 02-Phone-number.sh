fb79c8d9d69a3aa151ad08cf78f42e6808078350
balamuralich <balamuralikrishnach@gmail.com>
2025-09-27 12:44:05 +0530

    changed code

--- 02-Phone-number.sh
+++ 02-Phone-number.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-each "Please enter your Mobile Number"
+echo "Please enter your Mobile Number"
 read -s Mobile_Number
 
-each "Mobile Number entered is: $Mobile_Number"
+echo "Mobile Number entered is: $Mobile_Number"
 
