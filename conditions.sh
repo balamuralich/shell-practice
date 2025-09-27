a65dc80295b57266c06bd80db32324b508aee634
balamuralich <jyobalach@gmail.com>
2025-09-28 02:46:58 +0530

    changed code

--- conditions.sh
+++ conditions.sh
@@ -3,8 +3,7 @@
 NUMBER=$1
 
 if [ $NUMBER is -lt 5 ]; then
-    echo "Givent number $NUMBER is less than 5"
+  echo "Givent number $NUMBER is less than 5"
 else
-    echo "Given number $NUMBER is greater than 5"
-
+   echo "Given number $NUMBER is greater than 5"
 fi
