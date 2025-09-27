094924c9a76a9f52468e003fbadb553eca60ca93
balamuralich <jyobalach@gmail.com>
2025-09-28 03:00:04 +0530

    changed code

--- odd_even.sh
+++ odd_even.sh
@@ -4,8 +4,8 @@ echo "Please enter your number: "
 
 read NUMBER
 
-if [$(( $NUMBER %2 )) -eq 0] then
-    echo "Given number is EVEN"
+if [$(($NUMBER %2)) -eq 0]; then
+   echo "Given number is EVEN"
 else
-    echo "Given number is ODD"
+   echo "Given number is ODD"
 fi
