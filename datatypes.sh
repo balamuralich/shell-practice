6ef95089b2d78226059bd51c9069c2a712b80a61
balamuralich <jyobalach@gmail.com>
2025-09-28 02:03:23 +0530

    changed code

--- datatypes.sh
+++ datatypes.sh
@@ -8,3 +8,7 @@ SUM=$(($NUMBER1+$NUMBER2))
 
 echo "The total is $SUM"
 echo "This number belongs to $1"
+
+HERO_NAMES=("Pawan" "Mahesh" "Nani" "Ramcharan")
+
+echo "Tollywood Heroes ${HERO_NAMES[@]}"
