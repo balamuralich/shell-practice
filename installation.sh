b4d79731199dc47bdec6092329cbc32e6c33eda3
balamuralich <jyobalach@gmail.com>
2025-09-28 03:31:06 +0530

    changed code

--- installation.sh
+++ installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR :: Please run with root previlege"
+    exit 1
 fi
 
 dnf install Mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR :: Installing MySQL is failure"
+    exit 1
 else
     echo "SUCCESS :: Installing MySQL is completed"
 fi
