0337b4015edb88e759474db9931382a0baefd12e
balamuralich <jyobalach@gmail.com>
2025-10-01 02:48:48 +0530

    changed code

---
+++ RUN.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Color codes
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+B="\e[1m"   # Bold
+N1="\e[22m" # No Bold (use \e[22m instead of \e[0m to disable bold only)
+
+# Check for root privileges
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e "${R}${B}ERROR${N1}${N} - Please run with root privileges"
+    exit 1
+fi
+
+# Validation function
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "${R}${B}ERROR${N1}${N} - Command failed"
+        exit 1
+    else
+        echo -e "${G}${B}SUCCESS${N1}${N}"
+    fi
+}
+
+# Check if MySQL is installed
+dnf list installed mysql &>/dev/null
+if [ $? -ne 0 ]; then
+    echo -e "${Y}${B}Installing MySQL...${N1}${N}"
+    dnf install mysql -y
+    VALIDATE $?
+else
+    echo -e "MySQL already installed..... ${Y}${B}SKIPPING${N1}${N}"
+fi
