71aabaa333a19d8bc0adf04686d0b1bd4af3a42a
balamuralich <jyobalach@gmail.com>
2025-10-01 02:37:29 +0530

    changed code

--- Practice-2-Ifcondition.sh
+++ Practice-2-Ifcondition.sh
@@ -7,27 +7,25 @@ N="\e[0m"
 B="\e[1m" #Bold
 N1="\e[0m" #No Bold
 
-USERID=(id -u)
+USERID=$(id -u)
 
-if [ $USERID -ne 0 ]; then
-    echo -e "$R $B ERROR $N1 $N - Please run from root previlege"
+if [ "$USERID" -ne 0 ]; then
+    echo -e "$R $B ERROR $N1 $N - Please run with root privileges"
     exit 1
 fi
 
 VALIDATE(){
-
-    if [ $1 -ne 0 ]; then
-    echo -e "$R$B ERROR $N1$N - Please run from root previlege"
-    exit 1
-else
-    echo -e "$G$B SUCCESS $N1$N"
-fi
-
+    if [ "$1" -ne 0 ]; then
+        echo -e "$R$B ERROR $N1$N - Command failed"
+        exit 1
+    else
+        echo -e "$G$B SUCCESS $N1$N"
+    fi
 }
 
 dnf list installed mysql
 
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
     dnf install mysql -y
     VALIDATE $?
 else
