f5d523aead0c210227d9ed70fbbc109a0741d2e2
balamuralich <jyobalach@gmail.com>
2025-10-01 00:40:28 +0530

    newone

--- Practice-1.sh
+++ Practice-1.sh
@@ -4,5 +4,10 @@ PERSON1=$1
 PERSON2=$2
 PERSON3=$3
 
-echo "Once upon a time $1 told to $2 very confidently Hey $2 I will be the best one day in the world on that day $3 will be very happy. Then $2 replied I am also praying about that only"
+Red=$\e[0;31m
+Green=$\e[0;32m
+Yellow=$\e[0;33m
+Nocolor=$\e[0;37m
+
+echo "Once upon a time $Red $1 $Nocolor told to $2 very confidently Hey $2 I will be the best one day in the world on that day $3 will be very happy. Then $2 replied I am also praying about that only"
 echo "After verfification $1, $2, $3 are brothers and $1 no more now. But $2 and $3 are going with $1's thoughts."
