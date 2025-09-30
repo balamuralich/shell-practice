aa81a7e0f957e68f77157dad756489ac20eb8114
balamuralich <jyobalach@gmail.com>
2025-10-01 02:02:25 +0530

    changed code

--- Practice-2-Ifcondition.sh
+++ Practice-2-Ifcondition.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-P1=$1
-P2=$2
+R="\e[0;31m" #Red color
+G="\e[0;32m" #Green color
+Y="\e[0;33m" #Yellow color
+N="\e[0;37m" #No color
+B="\e[1m" #Bold
+N1="\e[0m" #No Bold
 
-if [ $1 -eq 0 ]; then
-    echo "$2 is Black"
+USERID=($id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "$R$BERROR$N1$N - Please run from root previlege"
+    exit1
+else
+    echo "$G$BSUCCESS$N1$N"
+fi
+
+VALIDATE(){
+
+    if [ $USERID -ne 0 ]; then
+    echo "$R$BERROR$N1$N - Please run from root previlege"
+    exit1
+else
+    echo "$G$BSUCCESS$N1$N"
+fi
+
+}
+
+dnf installed mysql -y
+
+if [ $? -eq 0]; then
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo "MySQL already installed..... $Y$B SKIPPING $N1$N
 fi
