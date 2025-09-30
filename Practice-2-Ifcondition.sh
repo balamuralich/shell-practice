366d5b16826bdf34186e5704616ec70b9e77034f
balamuralich <jyobalach@gmail.com>
2025-10-01 02:28:20 +0530

    changed code

--- Practice-2-Ifcondition.sh
+++ Practice-2-Ifcondition.sh
@@ -7,18 +7,18 @@ N="\e[0m"
 B="\e[1m" #Bold
 N1="\e[0m" #No Bold
 
-USERID=($id -u)
+USERID=(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R $B ERROR $N1 $N - Please run from root previlege"
-    exit1
+    exit 1
 fi
 
 VALIDATE(){
 
     if [ $1 -ne 0 ]; then
     echo -e "$R$B ERROR $N1$N - Please run from root previlege"
-    exit1
+    exit 1
 else
     echo -e "$G$B SUCCESS $N1$N"
 fi
@@ -27,9 +27,9 @@ fi
 
 dnf list installed mysql
 
-if [ $? -eq 0]; then
+if [ $? -eq 0 ]; then
     dnf install mysql -y
     VALIDATE $? "MySQL"
 else
-    echo -e "MySQL already installed..... $Y$B SKIPPING $N1$N
+    echo -e "MySQL already installed..... $Y$B SKIPPING $N1$N"
 fi
