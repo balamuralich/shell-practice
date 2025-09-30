a19ca076b802defd790b0ef441e8f10c98aa8889
balamuralich <jyobalach@gmail.com>
2025-10-01 02:21:59 +0530

    changed code

--- Practice-2-Ifcondition.sh
+++ Practice-2-Ifcondition.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-R="\e[0;31m" #Red color
-G="\e[0;32m" #Green color
-Y="\e[0;33m" #Yellow color
-N="\e[0m" #No color
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 B="\e[1m" #Bold
 N1="\e[0m" #No Bold
 
@@ -17,7 +17,7 @@ fi
 VALIDATE(){
 
     if [ $1 -ne 0 ]; then
-    echo -e "$R$ BERROR $N1$N - Please run from root previlege"
+    echo -e "$R$B ERROR $N1$N - Please run from root previlege"
     exit1
 else
     echo -e "$G$B SUCCESS $N1$N"
