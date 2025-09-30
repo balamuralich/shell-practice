56cd7b51d65556a82703533f13573ed23052fa47
balamuralich <jyobalach@gmail.com>
2025-10-01 01:40:04 +0530

    changed code

--- Practice-1.sh
+++ Practice-1.sh
@@ -4,12 +4,12 @@ PERSON1=$1
 PERSON2=$2
 PERSON3=$3
 
-Red="\e[0;31m"
-Green="\e[0;32m"
-Yellow="\e[0;33m"
-Nocolor="\e[0;37m"
-Bold="\e[1m"
-Nobold="\e[0m"
+R="\e[0;31m" #Red color
+G="\e[0;32m" #Green color
+Y="\e[0;33m" #Yellow color
+N="\e[0;37m" #No color
+B="\e[1m" #Bold
+N1="\e[0m" #No Bold
 
-echo -e "Once upon a time $Bold$Red $1 $Nocolor$Nobold told to $2 very confidently Hey $2 I will be the best one day in the world on that day $3 will be very happy. Then $2 replied I am also praying about that only"
+echo -e "Once upon a time$B$R $1 $N$N1 told to $2 very confidently Hey $2 I will be the best one day in the world on that day $3 will be very happy. Then $2 replied I am also praying about that only"
 echo "After verfification $1, $2, $3 are brothers and $1 no more now. But $2 and $3 are going with $1's thoughts."
